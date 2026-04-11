ae61774cccd37093f8bff72514f966df7b0312b9
gustavorods <gustavo.rlsilva07@gmail.com>
2026-04-10 21:47:30 -0300

    feat: add visual separators in console output for better readability

--- Main.java
+++ Main.java
@@ -1,56 +1,55 @@
 public class Main {
 
-    // Método principal do programa
-    // Todo programa Java começa a execução pelo método main
     public static void main(String[] args) {
 
-        // Criação de um objeto Produto usando o construtor com parâmetros
-        Produto p = new Produto("Notebook", 3500, 5);
+        // =========================
+        // BLOCO: PRODUTO
+        // =========================
+        System.out.println("\n=========================");
+        System.out.println("       PRODUTO");
+        System.out.println("=========================");
 
-        // Chamada do método que exibe as informações do produto
+        Produto p = new Produto("Notebook", 3500, 5);
         p.exibirInformacoes();
 
 
-        // Criação de um vetor (array) do tipo Veiculo com 2 posições
-        // Ele pode armazenar objetos de classes que herdam de Veiculo
+        // =========================
+        // BLOCO: VEÍCULOS
+        // =========================
+        System.out.println("\n=========================");
+        System.out.println("       VEÍCULOS");
+        System.out.println("=========================");
+
         Veiculo[] veiculos = new Veiculo[2];
-
-        // Criando um objeto Carro e armazenando na posição 0 do vetor
         veiculos[0] = new Carro("Toyota", "Corolla", 2022, 4);
-
-        // Criando um objeto Moto e armazenando na posição 1 do vetor
         veiculos[1] = new Moto("Honda", "CB500", 2023, 500);
 
-        // Estrutura de repetição "for-each"
-        // Percorre todos os elementos do vetor veiculos
-        for(Veiculo v : veiculos){
-
-            // Chama o método exibirDados()
-            // Cada objeto executará sua própria versão do método
-            // (conceito de polimorfismo)
+        for (Veiculo v : veiculos) {
             v.exibirDados();
         }
 
 
-        // Criação de um objeto Curso
+        // =========================
+        // BLOCO: ALUNO E CURSO
+        // =========================
+        System.out.println("\n=========================");
+        System.out.println("     ALUNO / CURSO");
+        System.out.println("=========================");
+
         Curso c = new Curso("ADS");
-
-        // Criação de um objeto Aluno que recebe um Curso
-        // Exemplo de relacionamento entre classes (associação)
         Aluno a = new Aluno("Fernando", c);
-
-        // Exibe os dados do aluno
         a.exibirDados();
 
 
-        // Criação de um objeto da classe Calculadora
+        // =========================
+        // BLOCO: CALCULADORA
+        // =========================
+        System.out.println("\n=========================");
+        System.out.println("     CALCULADORA");
+        System.out.println("=========================");
+
         Calculadora calc = new Calculadora();
-
-        // Chamada do método somar usando inteiros
-        System.out.println(calc.somar(5,3));
-
-        // Chamada do método somar usando números decimais
-        // Exemplo de sobrecarga de métodos (mesmo método com parâmetros diferentes)
-        System.out.println(calc.somar(5.5,2.5));
+        System.out.println(calc.somar(5, 3));
+        System.out.println(calc.somar(5.5, 2.5));
     }
 }
